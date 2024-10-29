b15cdd14e0a03e79c676624cc2cc05147bc76fce
BrunoOliveira90 <brunowowps@gmail.com>
2024-10-29 20:14:21 -0300

    Updating files

--- app/__init__.py
+++ app/__init__.py
@@ -31,6 +31,8 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Please log in to access this page.'
+    login_manager.login_message_category = 'danger'
     login_manager.init_app(app)
 
     @login_manager.user_loader
